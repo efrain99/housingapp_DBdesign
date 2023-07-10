3d627ac9e3f5cf7b1e0aa290477e83e943fffe42
efrain99 <ecp5kf@virginia.edu>
2023-07-10 00:08:23 -0400

    adding more tables

--- housingDB.sql
+++ housingDB.sql
@@ -1,6 +1,8 @@
 CREATE SCHEMA housing_data
     CREATE TABLE housing_data.country(
         id SERIAL PRIMARY KEY NOT NULL,
+        name_country VARCHAR(20) NOT NULL
+        date_month DATE NOT NULL,
         median_list_price INT NOT NULL,
         active_list_count INT NOT NULL,
         median_day_market INT NOT NULL,
@@ -8,12 +10,13 @@ CREATE SCHEMA housing_data
         pending_count INT NOT NULL,
         average_list_price INT NOT NULL,
         median_square_feet INT NOT NULL,
-        price_reduced_count INT NOT NULL,
-        date_month DATE NOT NULL,
-        name_country VARCHAR(20) NOT NULL)
+        price_reduced_count INT NOT NULL
+    )
 
     CREATE TABLE housing_data.states(
         id SERIAL PRIMARY KEY NOT NULL,
+        date_month DATE NOT NULL,
+        name_state VARCHAR(20) NOT NULL,
         median_list_price INT NOT NULL,
         active_list_count INT NOT NULL,
         median_day_market INT NOT NULL,
@@ -22,7 +25,36 @@ CREATE SCHEMA housing_data
         average_list_price INT NOT NULL,
         median_square_feet INT NOT NULL,
         price_reduced_count INT NOT NULL,
+        quality_flag BIT(1) NOT NULL
+    )
+
+    CREATE TABLE housing_data.metro_city(
+        id SERIAL PRIMARY KEY NOT NULL,
         date_month DATE NOT NULL,
+        name_city VARCHAR(30) NOT NULL,
+        median_list_price INT NOT NULL,
+        active_list_count INT NOT NULL,
+        median_day_market INT NOT NULL,
+        price_increase_count INT NOT NULL,
+        pending_count INT NOT NULL,
+        average_list_price INT NOT NULL,
+        median_square_feet INT NOT NULL,
+        price_reduced_count INT NOT NULL,
+        quality_flag BIT(1) NOT NULL
+    )
+
+    CREATE TABLE housing_data.county(
+        id SERIAL PRIMARY KEY NOT NULL,
+        date_month DATE NOT NULL,
+        name_county VARCHAR(30) NOT NULL,
         name_state VARCHAR(20) NOT NULL,
+        median_list_price INT NOT NULL,
+        active_list_count INT NOT NULL,
+        median_day_market INT NOT NULL,
+        price_increase_count INT NOT NULL,
+        pending_count INT NOT NULL,
+        average_list_price INT NOT NULL,
+        median_square_feet INT NOT NULL,
+        price_reduced_count INT NOT NULL,
         quality_flag BIT(1) NOT NULL
     )
